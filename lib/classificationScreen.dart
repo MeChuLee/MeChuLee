ee98702126f5af17eb74d188e9c704f62cc15b7c
JungWooGeon <jwk1734@naver.com>
2023-04-03 00:10:28 +0900

    메인화면 구현, 6가지 기준 화면 추가

---
+++ lib/classificationScreen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class ClassificationScreen extends StatelessWidget {
+  const ClassificationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "ClassificationScreen",
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xffffd864),
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back, color: Colors.black),
+            ),
+            title: const Text("음식 분류", style: TextStyle(color: Colors.black)),
+          ),
+          bottomNavigationBar: Container(
+            height: 60,
+            color: const Color(0x99ffd648),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                    child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          height: 60,
+                          alignment: Alignment.center,
+                          child:
+                              const Text("취소", style: TextStyle(fontSize: 18)),
+                        ))),
+                const VerticalDivider(
+                    color: Color.fromARGB(255, 211, 211, 211), thickness: 1.0),
+                Expanded(
+                    child: InkWell(
+                        onTap: () {
+                          //@TODO '선택' 버튼 클릭 이벤트
+                        },
+                        child: Container(
+                          height: 60,
+                          alignment: Alignment.center,
+                          child:
+                              const Text("선택", style: TextStyle(fontSize: 18)),
+                        ))),
+              ],
+            ),
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mechulee/classificationScreen.dart';
+import 'package:mechulee/costScreen.dart';
+import 'package:mechulee/preferenceScreen.dart';
+import 'package:mechulee/randomScreen.dart';
+import 'package:mechulee/restrictionsScreen.dart';
+import 'package:mechulee/situationScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,72 +16,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: const Color(0x33d9d9d9)),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color(0xffffd864),
-          leading: const Icon(Icons.menu, color: Colors.black),
-          title: const Text("메추리", style: TextStyle(color: Colors.black)),
-          actions: [Image.asset("logo.png")],
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 0,
-          childAspectRatio: 1 / 1,
-          children: <Widget>[
-            Card(
-              color: Colors.white,
-              margin: const EdgeInsets.fromLTRB(20, 20, 10, 10),
-              elevation: 4.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+        debugShowCheckedModeBanner: false,
+        title: "MeChuLee",
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: const Color(0xffffd864),
+            leading: IconButton(
+              icon: const Icon(Icons.menu, color: Colors.black),
+              onPressed: () {},
+            ),
+            title: const Text("메추리", style: TextStyle(color: Colors.black)),
+            actions: [
+              IconButton(
+                icon: Image.asset("assets/logo.png"),
+                onPressed: () {},
               )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 10,
+              childAspectRatio: 1 / 1,
+              children: <Widget>[
+                MyCard("비용", "돈 아껴야 돼~", "assets/money.png", 0),
+                MyCard("식단 제약", "편식 ㄱㄱ", "assets/salad.png", 1),
+                MyCard("랜덤", "운세를 보라", "assets/shuffle.png", 2),
+                MyCard("개인 선호도", "뭐가 좋니?", "assets/like.png", 3),
+                MyCard("개인 상황", "렛츠고 피크닉", "assets/sun.png", 4),
+                MyCard("음식 분류", "한식 중식 일식?", "assets/dish.png", 5),
+              ],
             ),
-            Card(
-                color: Colors.white,
-                margin: const EdgeInsets.fromLTRB(10, 20, 20, 10),
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )
+          ),
+        ));
+  }
+}
+
+class MyCard extends StatelessWidget {
+  final String title;
+  final String content;
+  final String img;
+  final int idx;
+  final screenList = <StatelessWidget>[
+    const CostScreen(),
+    const RestrictionsScreen(),
+    const RandomScreen(),
+    const PreferenceScreen(),
+    const SituationScreen(),
+    const ClassificationScreen(),
+  ];
+
+  MyCard(this.title, this.content, this.img, this.idx, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InkWell(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => screenList[idx]));
+          },
+          child: Card(
+            color: Colors.white,
+            elevation: 4.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-            Card(
-                color: Colors.white,
-                margin: const EdgeInsets.fromLTRB(20, 10, 10, 10),
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(18, 18, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                      child: Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Text(content,
+                        style: const TextStyle(
+                          fontSize: 12,
+                        )),
+                  ),
+                  Flexible(fit: FlexFit.tight, child: Image.asset(img))
+                ],
+              ),
             ),
-            Card(
-                color: Colors.white,
-                margin: const EdgeInsets.fromLTRB(10, 10, 20, 10),
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )
-            ),
-            Card(
-                color: Colors.white,
-                margin: const EdgeInsets.fromLTRB(20, 10, 10, 10),
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )
-            ),
-            Card(
-                color: Colors.white,
-                margin: const EdgeInsets.fromLTRB(10, 10, 20, 10),
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )
-            ),
-          ],
-        ),
-      )
+          )),
     );
   }
 }
